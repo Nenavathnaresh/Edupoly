9f7124b3876cb3f6527fee8b243a33fb31b72dc1
Nenavath Naresh <nenavathnaresh460@gmail.com>
2024-03-01 17:21:30 +0530

    dfgh

---
+++ React/Restaurant table booking project/table.js
@@ -0,0 +1,154 @@
+ var Alltables = [
+    {
+        'id': 'T1',
+        'numOfSeats': 4,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+    {
+        'id': 'T2',
+        'numOfSeats': 2,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+       
+    },
+    {
+        'id': 'T3',
+        'numOfSeats': 6,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+    {
+        'id': 'T4',
+        'numOfSeats': 4,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+    {
+        'id': 'T5',
+        'numOfSeats': 6,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+    {
+        'id': 'T6',
+        'numOfSeats': 8,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+    {
+        'id': 'T7',
+        'numOfSeats': 2,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+    {
+        'id': 'T8',
+        'numOfSeats': 4,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+    {
+        'id': 'T9',
+        'numOfSeats': 6,
+        'isOccupied': false,
+        'items':[],
+        'previousBookings':[]
+    },
+ ]
+
+ var menuItems=[
+    {
+        name:"Veg Manchuriya",
+        price:140
+    },
+    {
+        name:"Egg Manchuriya",
+        price:150
+    },
+    {
+        name:"Double Egg Manchuriya",
+        price:160
+    },
+    {
+        name:"Chicken Manchuriya",
+        price:180
+    },
+    {
+        name:"Veg Noodles",
+        price:140
+    },
+    {
+        name:"Egg Noodles",
+        price:150
+    },
+    {
+        name:"Double Egg Noodles",
+        price:160
+    },
+    {
+        name:"Chicken Noodles",
+        price:180
+    },
+    {
+        name:"Veg Fried Rice",
+        price:140
+    },
+    {
+        name:"Egg Fried Rice",
+        price:150
+    },
+    {
+        name:"Double Egg Fried Rice",
+        price:160
+    },
+    {
+        name:"Chicken Fried Rice",
+        price:180
+    },
+    {
+        name:"Veg meals",
+        price:200
+    },
+    {
+        name:"Non Veg Meals",
+        price:280
+    },
+    {
+        name:"Chicken Dum Biryani",
+        price:280
+    },
+    {
+        name:"Chicken Fry Piece Biryani",
+        price:320
+    },
+    {
+        name:"Chicken Spl Biryani",
+        price:320
+    },
+    {
+        name:"Chicken Mughalai Biryani",
+        price:320
+    },
+    {
+        name:"Zaffrani Chicken Biryani",
+        price:350
+    },
+    {
+        name:"Prawns Biryani",
+        price:350
+    },
+    {
+        name:"Fish Fry Biryani",
+        price:350
+    },
+    
+]
